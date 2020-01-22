18d5975b738d96f45d7a23318b95f68587efaefd
petermerrill <petejmerrill@gmail.com>
2020-01-22 12:55:43 +0000

    Generated code by SwaggerHub

--- src/main/scala/io/swagger/server/model/BrandedFoodObject_serving.scala
+++ src/main/scala/io/swagger/server/model/BrandedFoodObject_serving.scala
@@ -4,14 +4,12 @@ package io.swagger.server.model
 /**
  * An object containing serving information for this item
  *
- * @param size Serving size
- * @param measurement_unit Serving measurement unit (e.g. if measure is 3 tsp, the unit is tsp)
+ * @param size Serving size with measurement unit (e.g. if measure is 3 tsp, the unit is tsp)
  * @param size_fulltext Serving size description
  * @param total Total servings
  */
 case class BrandedFoodObject_serving (
-  size: Option[Int],
-  measurement_unit: Option[String],
+  size: Option[String],
   size_fulltext: Option[String],
   total: Option[Int]
 )
